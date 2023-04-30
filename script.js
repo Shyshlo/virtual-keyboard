a3f77ad7793e88fa702482a726e3dbf84cfd98ad
Liubou <slv5555555@mail.ru>
2023-04-30 22:41:37 +0300

    refactor: add functions for check language and for key in script.js

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ function getMain() {
 getMain();
 
 
-
 const keys1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
 const keys2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete'];
 const keys3 = ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'];
@@ -76,5 +75,114 @@ const kCRuS = {
 };
 
 
+let shiftSwitch = false;
+const DefLang = 'en';
+let lang;
+let langTogle = false;
+let capsTogle = false;
+const OKEn = Object.keys(keyCodEn);
+const OKEnS = Object.keys(kCEnS);
+const OKRu = Object.keys(keyCodRu);
+const OKRuS = Object.keys(kCRuS);
 
+if (localStorage.getItem('lang') === null) {
+  lang = DefLang;
+} else if (localStorage.getItem('lang') !== null) {
+  lang = localStorage.getItem('lang');
+}
+
+
+
+function getKeys(Arr1, Arr2, Arr3, Arr4, Arr5) {
+    function getKeysOne(keyArr1) {
+      let key1 = '';
+      for (let i = 0; i < keyArr1.length; i += 1) {
+        if (keyArr1[i] === 'Backspace') {
+          key1 += `<div class="button Backspace special">${keyArr1[i]}</div>`;
+        } else {
+          key1 += `<div class="button">${keyArr1[i]}</div>`;
+        }
+      }
+      document.querySelector('.row1').innerHTML = key1;
+    }
   
+    getKeysOne(Arr1);
+  
+    function getKeysTwo(keyArr2) {
+      let key2 = '';
+      for (let i = 0; i < keyArr2.length; i += 1) {
+        if (keyArr2[i] === 'Tab') {
+          key2 += `<div class="button Tab special">${keyArr2[i]}</div>`;
+        } else {
+          key2 += `<div class="button">${keyArr2[i]}</div>`;
+        }
+      }
+      document.querySelector('.row2').innerHTML = key2;
+    }
+  
+    getKeysTwo(Arr2);
+  
+    function getKeysThree(keyArr3) {
+      let key3 = '';
+      for (let i = 0; i < keyArr3.length; i += 1) {
+        if (keyArr3[i] === 'CapsLock') {
+          key3 += `<div class="button CapsLock special">${keyArr3[i]}</div>`;
+        } else if (keyArr3[i] === 'Enter') {
+          key3 += `<div class="button Enter special">${keyArr3[i]}</div>`;
+        } else {
+          key3 += `<div class="button">${keyArr3[i]}</div>`;
+        }
+      }
+      document.querySelector('.row3').innerHTML = key3;
+    }
+  
+    getKeysThree(Arr3);
+  
+    function getKeysFour(keyArr4) {
+      let key4 = '';
+      for (let i = 0; i < keyArr4.length; i += 1) {
+        if (keyArr4[i] === 'ShiftLeft') {
+          key4 += '<div class="button ShiftLeft special">Shift</div>';
+        } else if (keyArr4[i] === 'ShiftRight') {
+          key4 += '<div class="button ShiftRight special">Shift</div>';
+        } else {
+          key4 += `<div class="button">${keyArr4[i]}</div>`;
+        }
+      }
+      document.querySelector('.row4').innerHTML = key4;
+    }
+  
+    getKeysFour(Arr4);
+  
+    function getKeysFive(keyArr5) {
+      let key5 = '';
+      for (let i = 0; i < keyArr5.length; i += 1) {
+        if (keyArr5[i] === ' ') {
+          key5 += `<div class="button Space special">${keyArr5[i]}</div>`;
+        } else if (keyArr5[i] === 'AltRight') {
+          key5 += '<div class="button AltRight special">Alt</div>';
+        } else if (keyArr5[i] === 'ControlRight') {
+          key5 += '<div class="button ControlRight special">Ctrl</div>';
+        } else if (keyArr5[i] === 'AltLeft') {
+          key5 += '<div class="button AltLeft special">Alt</div>';
+        } else if (keyArr5[i] === 'ControlLeft') {
+          key5 += '<div class="button ControlLeft special">Ctrl</div>';
+        } else {
+          key5 += `<div class="button">${keyArr5[i]}</div>`;
+        }
+      }
+      document.querySelector('.row5').innerHTML = key5;
+    }
+  
+    getKeysFive(Arr5);
+  }
+  
+  function getLanguage() {
+    if (lang === 'en') {
+      getKeys(keys1, keys2, keys3, keys4, keys5);
+    } else if (lang === 'ru') {
+      getKeys(keysRu1, keysRu2, keysRu3, keysRu4, keysRu5);
+    }
+  }
+  
+  getLanguage();
